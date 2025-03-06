bebdbc4850038aa565581ce7d2bca474d6be0e70
Tobias Bordenca <tobiasuzt@gmail.com>
2025-03-06 10:42:42 +0100

    Update EPFL logo

--- src/data/educationData.ts
+++ src/data/educationData.ts
@@ -6,7 +6,7 @@ export const educationData = [
     period: '2017 - 2019',
     description:
       "Focused on product management, entrepreneurship, and technology strategy. Participated in the university's startup incubator program and led a team that won the annual business plan competition.",
-    logo: 'https://www.epfl.ch/about/overview/wp-content/uploads/2020/07/logo-epfl-1024x576.png',
+    logo: 'https://www.epfl.ch/wp-content/themes/wp-theme-2018/assets/svg/epfl-logo.svg',
   },
   {
     institution: 'EPFL (Swiss Federal Institute of Technology in Lausanne)',
@@ -15,6 +15,6 @@ export const educationData = [
     period: '2013 - 2016',
     description:
       'Graduated with honors. Coursework included software engineering, human-computer interaction, and artificial intelligence. Completed a senior project on predictive analytics for e-commerce.',
-    logo: 'https://www.epfl.ch/about/overview/wp-content/uploads/2020/07/logo-epfl-1024x576.png',
+    logo: 'https://www.epfl.ch/wp-content/themes/wp-theme-2018/assets/svg/epfl-logo.svg',
   },
 ];
